37b4c69fdbcb48f98d30c39542eace7c481354b5
Ulrich Habel <espero7757@gmx.net>
2020-07-19 14:47:05 +0200

    SW - V2 - IIC for LCD works

--- SW/vitis_workspace/main/src/freertos_hello_world.c
+++ SW/vitis_workspace/main/src/freertos_hello_world.c
@@ -25,22 +25,23 @@
 #include "xil_printf.h"
 
 
-#define TIMER_ID				1
-#define DELAY_10_SECONDS		10000UL
-#define DELAY_1_SECOND			1000UL
-#define DELAY_1000_MILLISEC		1000UL
-#define DELAY_100_MILLISEC		100UL
-#define TIMER_CHECK_THRESHOLD	9
+#define TIMER_ID					1
+#define DELAY_10_SECONDS			10000UL
+#define DELAY_1_SECOND				1000UL
+#define DELAY_1000_MILLISEC			1000UL
+#define DELAY_100_MILLISEC			100UL
+#define TIMER_CHECK_THRESHOLD		9
 
 
 /* XPAR_IIC_1_DEVICE_ID  :=  XPAR_AXI_IIC_0_DEVICE_ID */
-#define IIC_PLL_BASE_ADDRESS	XPAR_IIC_1_BASEADDR
-#define IIC_PLL_ONCHIP_ADDRESS 	0x70
+#define IIC_PLL_BASE_ADDRESS		XPAR_IIC_1_BASEADDR
+#define IIC_PLL_ONCHIP_ADDRESS 		0x70
 
 /* XPAR_IIC_0_DEVICE_ID  :=  XPAR_AXI_BOARD_IIC_0_DEVICE_ID */
-#define IIC_BOARD_BASE_ADDRESS	XPAR_IIC_0_BASEADDR
-#define IIC_DAC_ONCHIP_ADDRESS 	0x1C
-#define IIC_LCD_ONCHIP_ADDRESS 	0x2A
+#define IIC_BOARD_BASE_ADDRESS		XPAR_IIC_0_BASEADDR
+#define IIC_DAC_ONCHIP_ADDRESS 		0x1c
+#define IIC_DAC_BROADCAST_ADDRESS	0x2a
+#define IIC_LCD_ONCHIP_ADDRESS 		0x3e
 
 
 /*-----------------------------------------------------------*/
@@ -132,56 +133,192 @@ int main( void )
 
 
 /*-----------------------------------------------------------*/
+
+static u8 lcdTextWrite(u8 row, u8 col, u8 strLen, const char* strBuf)
+{
+    /* Sanity checks */
+    if (row > 1 || col > 15) {
+      return XST_FAILURE;
+    }
+
+    /* Write Text */
+    for (u8 txtIdx = 0U; txtIdx < strLen; ++txtIdx) {
+    	const u8 cursorPos = 0x7fU & (row * 40U + col++);
+    	u32 ByteCount;
+    	u8  iicData[2];
+
+    	for (u8 rptIdx = 0U; rptIdx < 1U; rptIdx++) {
+			/* Set Position */
+			iicData[0] 	= 0x00U;
+			iicData[1] 	= 0x80U | cursorPos;
+			ByteCount 	= 2;
+			ByteCount = XIic_Send(IIC_BOARD_BASE_ADDRESS, IIC_LCD_ONCHIP_ADDRESS, (u8*) &iicData, ByteCount, XIIC_STOP);
+
+			/* Set Character */
+			iicData[0] 	= 0x40U;
+			iicData[1] 	= *(strBuf + txtIdx);
+			ByteCount 	= 2;
+			ByteCount = XIic_Send(IIC_BOARD_BASE_ADDRESS, IIC_LCD_ONCHIP_ADDRESS, (u8*) &iicData, ByteCount, XIIC_STOP);
+    	}
+    }
+    return XST_SUCCESS;
+}
+
 static void prvTxTask( void *pvParameters )
 {
-	const TickType_t ticks100ms  = pdMS_TO_TICKS(DELAY_100_MILLISEC);
-	//const TickType_t ticks1000ms = pdMS_TO_TICKS(DELAY_1000_MILLISEC);
-	//u8 loopIdx = 0U;
-
 #if 1
-	int statusRotenc = XGpio_Initialize(&Gpio_Rotenc, XPAR_ROTENC_DECODER_AXI_ROTENC_GPIO_0_DEVICE_ID);
-	if (statusRotenc != XST_SUCCESS) {
-		xil_printf("GPIO Rotenc Initialization Failed\r\n");
-		return;
+	/* ROTENC counter and push button */
+	{
+		int statusRotenc = XGpio_Initialize(&Gpio_Rotenc, XPAR_ROTENC_DECODER_AXI_ROTENC_GPIO_0_DEVICE_ID);
+		if (statusRotenc != XST_SUCCESS) {
+			xil_printf("GPIO Rotenc Initialization Failed\r\n");
+			return;
+		}
+		XGpio_SetDataDirection(&Gpio_Rotenc, 1U, 0xffffffffUL);  // 32 bit input
+		XGpio_SetDataDirection(&Gpio_Rotenc, 2U, 0x0000ffffUL);  // 16 bit output - 16 bit input
 	}
-	XGpio_SetDataDirection(&Gpio_Rotenc, 1U, 0xffffffffUL);  // 32 bit input
-	XGpio_SetDataDirection(&Gpio_Rotenc, 2U, 0x0000ffffUL);  // 16 bit output - 16 bit input
 #endif
 
 #if 1
-	int statusPwmLights = XGpio_Initialize(&Gpio_PWM_Lights, XPAR_PWM_LIGHTS_AXI_PWM_GPIO_0_DEVICE_ID);
-	if (statusPwmLights != XST_SUCCESS) {
-		xil_printf("GPIO PWM Lights Initialization Failed\r\n");
-		return;
+	/* PWM Lights */
+	{
+		int statusPwmLights = XGpio_Initialize(&Gpio_PWM_Lights, XPAR_PWM_LIGHTS_AXI_PWM_GPIO_0_DEVICE_ID);
+		if (statusPwmLights != XST_SUCCESS) {
+			xil_printf("GPIO PWM Lights Initialization Failed\r\n");
+			return;
+		}
+		XGpio_SetDataDirection(&Gpio_PWM_Lights, 1U, 0x00000000UL);  // 32 bit output
+		XGpio_SetDataDirection(&Gpio_PWM_Lights, 2U, 0xffffffffUL);  // 32 bit input
 	}
-	XGpio_SetDataDirection(&Gpio_PWM_Lights, 1U, 0x00000000UL);  // 32 bit output
-	XGpio_SetDataDirection(&Gpio_PWM_Lights, 2U, 0xffffffffUL);  // 32 bit input
 #endif
 
-#if 1
-	int ByteCount;
-	u8 iicData;
+#if 0
+	/* I2C address mapping list */
+	{
+		u32 ByteCount;
+		u8 iicData;
 
-	for (u8 iicChipAddr = 0; iicChipAddr <= 0x7fU; ++iicChipAddr) {
-		xil_printf("IIC Test iicChipAddr = 0x%02x --> ", iicChipAddr);
+		for (u8 iicChipAddr = 0; iicChipAddr <= 0x7fU; ++iicChipAddr) {
+			xil_printf("IIC Test iicChipAddr = 0x%02x --> ", iicChipAddr);
 
-		u16 StatusReg = XIic_ReadReg(IIC_BOARD_BASE_ADDRESS, XIIC_SR_REG_OFFSET);
-		if(!(StatusReg & XIIC_SR_BUS_BUSY_MASK)) {
-			ByteCount = XIic_Recv(IIC_BOARD_BASE_ADDRESS, iicChipAddr, &iicData, 1, XIIC_STOP);
-			if (ByteCount) {
-				xil_printf("found\r\n");
-			} else {
-				xil_printf("no\r\n");
+			u16 StatusReg = XIic_ReadReg(IIC_BOARD_BASE_ADDRESS, XIIC_SR_REG_OFFSET);
+			if(!(StatusReg & XIIC_SR_BUS_BUSY_MASK)) {
+				ByteCount = XIic_Recv(IIC_BOARD_BASE_ADDRESS, iicChipAddr, &iicData, 1, XIIC_STOP);
+				if (ByteCount) {
+					xil_printf("found\r\n");
+				} else {
+					xil_printf("no\r\n");
+				}
 			}
 		}
 	}
-
 #endif
 
+#if 1
+	/* DAC pull voltage */
+	{
+		u32 ByteCount;
+		u8 iicData[3] = { 0 };
+
+#if 1
+		/* Write DAC data */
+		xil_printf("IIC write DAC Addr = 0x%02x --> ", IIC_DAC_ONCHIP_ADDRESS);
+
+		u16 StatusReg = XIic_ReadReg(IIC_BOARD_BASE_ADDRESS, XIIC_SR_REG_OFFSET);
+		if(!(StatusReg & XIIC_SR_BUS_BUSY_MASK)) {
+			iicData[0]	= 0x01U;  // command: DAC_latch & CODE register
+			iicData[1]	= 0x7fU;  // MSB
+			iicData[2]	= 0x30U;  // LSB
+			ByteCount 	= 3;
+			ByteCount = XIic_Send(IIC_BOARD_BASE_ADDRESS, IIC_DAC_ONCHIP_ADDRESS, (u8*) &iicData, ByteCount, XIIC_STOP);
+			xil_printf("write: data cnt = %d\r\n", ByteCount);
+		}
+#endif
+
+#if 0
+		/* Read DAC data */
+		xil_printf("IIC read DAC Addr = 0x%02x --> ", IIC_DAC_ONCHIP_ADDRESS);
+
+		u16 StatusReg = XIic_ReadReg(IIC_BOARD_BASE_ADDRESS, XIIC_SR_REG_OFFSET);
+		if(!(StatusReg & XIIC_SR_BUS_BUSY_MASK)) {
+			iicData[0]	= 0x01;
+			ByteCount 	= 1;
+			ByteCount = XIic_Send(IIC_BOARD_BASE_ADDRESS, IIC_DAC_ONCHIP_ADDRESS, (u8*) &iicData, ByteCount, XIIC_REPEATED_START);
+
+			iicData[0]	= 0x00U;
+			iicData[1]	= 0x00U;
+			ByteCount = XIic_Recv(IIC_BOARD_BASE_ADDRESS, IIC_DAC_ONCHIP_ADDRESS, (u8*) &iicData, 2, XIIC_STOP);
+			if (ByteCount) {
+				xil_printf("read: data cnt = %d, data[0] = 0x%02x, data[1] = 0x%02x\r\n", ByteCount, iicData[0], iicData[1]);
+			} else {
+				xil_printf("read: no data.\r\n");
+			}
+		}
+#endif
+	}
+#endif
+
+#if 1
+	/* LCD */
+	{
+		/* Pull and release reset line of the LCD */
+		{
+			const u32 pwmLights_gpio2_prev  	= XGpio_DiscreteRead(&Gpio_PWM_Lights, 2U);
+			const u32 pwmLights_gpio2_lcd_rstn  = pwmLights_gpio2_prev & ~0x01UL;
+			const u32 pwmLights_gpio2_lcd_actv  = pwmLights_gpio2_prev |  0x01UL;
+
+			xil_printf("LCD reset: rstn --> actv\r\n");
+
+			XGpio_DiscreteWrite(&Gpio_PWM_Lights, 2U, pwmLights_gpio2_lcd_rstn);
+			vTaskDelay(pdMS_TO_TICKS(2));
+
+			XGpio_DiscreteWrite(&Gpio_PWM_Lights, 2U, pwmLights_gpio2_lcd_actv);
+			vTaskDelay(pdMS_TO_TICKS(50));
+		}
+
+		xil_printf("IIC LCD Addr = 0x%02x --> ", IIC_LCD_ONCHIP_ADDRESS);
+
+		u16 StatusReg = XIic_ReadReg(IIC_BOARD_BASE_ADDRESS, XIIC_SR_REG_OFFSET);
+		if(!(StatusReg & XIIC_SR_BUS_BUSY_MASK)) {
+			const u8 iicData1[2] = { 0x00, 0x38 };
+			XIic_Send(IIC_BOARD_BASE_ADDRESS, IIC_LCD_ONCHIP_ADDRESS, (u8*) &iicData1, sizeof(iicData1), XIIC_STOP);
+			vTaskDelay(pdMS_TO_TICKS(50));
+
+			const u8 iicData2[2] = { 0x00, 0x39 };
+			XIic_Send(IIC_BOARD_BASE_ADDRESS, IIC_LCD_ONCHIP_ADDRESS, (u8*) &iicData2, sizeof(iicData2), XIIC_STOP);
+			vTaskDelay(pdMS_TO_TICKS(10));
+
+			const u8 iicData3[12] = {
+					0x80U, 0x14U, 			/* Internal OSC frequency - 1/5 bias; Osc frequency: abt. 183 Hz */
+					0x80U, 0x74U,			/* 							Contrast: LSB 36 of [0..63] */
+					0x80U, 0x56U,			/* Icon: off; Booster: on; 	Contrast: MSB 36 of [0..63] */
+					0x80U, 0x6cU,			/* Follower: on; amplification ratio: 4 of [0..7] */
+					0x80U, 0x0cU,			/* Display: on; Cursor: off; Cursor position: off */
+					0x00U, 0x01U,			/* Clear display */
+			};
+			XIic_Send(IIC_BOARD_BASE_ADDRESS, IIC_LCD_ONCHIP_ADDRESS, (u8*) &iicData3, sizeof(iicData3), XIIC_STOP);
+			vTaskDelay(pdMS_TO_TICKS(10));
+
+			const u8 col 			= 0U;
+			const u8 rowL1			= 0U;
+			const char strBufL1[] 	= "* UFBmod  V2.x *";
+			const u8 strLenL1		= 16;
+			lcdTextWrite(rowL1, col, strLenL1, strBufL1);
+
+			const u8 rowL2			= 1U;
+			//const char strBufL2[] = "*Demo by DF4IAH*";
+			const char strBufL2[] 	= "Demo by HFT Labs";
+			const u8 strLenL2		= 16;
+			lcdTextWrite(rowL2, col, strLenL2, strBufL2);
+		}
+	}
+#endif
+
+	//u8 loopIdx = 0U;
 	for( ;; )
 	{
 		/* Delay */
-		vTaskDelay(ticks100ms);
+		vTaskDelay(pdMS_TO_TICKS(100));
 
 #if 1
 		const u32 gpio1_ctr  = XGpio_DiscreteRead(&Gpio_Rotenc, 1U);
@@ -194,19 +331,18 @@ static void prvTxTask( void *pvParameters )
 			u8  pwm_r_val 	= (u8) (0x03U & (gpio1_ctr >> 2));
 			u8  pwm_g_val 	= (u8) (0x03U & (gpio1_ctr >> 4));
 			u8  pwm_b_val 	= (u8) (0x03U & (gpio1_ctr >> 6));
-			u8  pwm_bl_val	= (u8) (0x03U & (gpio1_ctr >> 8));
+			u8  pwm_bl_val; //	= (u8) (0x03U & (gpio1_ctr >> 8));
+			    pwm_bl_val  = 0x7f;
 
 			if (!gpio2_push) {
-				pwm_r_val = 0x20;
-				pwm_g_val = 0x20;
-				pwm_b_val = 0x20;
-				pwm_bl_val = 0x20;
+				pwm_r_val  = 0x20;
+				pwm_g_val  = 0x20;
+				pwm_b_val  = 0x20;
+				pwm_bl_val = 0xff;
 			}
 
-			u32 pwm_bf    	= ((u32) pwm_bl_val << 24) | ((u32) pwm_b_val << 16) | ((u32) pwm_g_val << 8) | ((u32) pwm_r_val << 0);
-#endif
-
-#if 0
+			u32 pwm_bf = ((u32) pwm_bl_val << 24) | ((u32) pwm_b_val << 16) | ((u32) pwm_g_val << 8) | ((u32) pwm_r_val << 0);
+#else
 			u32 pwm_bf;
 
 			switch (loopIdx)
@@ -228,14 +364,12 @@ static void prvTxTask( void *pvParameters )
 				pwm_bf = 0xff000000UL;
 				break;
 			}
-#endif
 
-			XGpio_DiscreteWrite(&Gpio_PWM_Lights, 1U, pwm_bf);
-
-#if 0
 			++loopIdx;
 			loopIdx &= 0x03U;
 #endif
+
+			XGpio_DiscreteWrite(&Gpio_PWM_Lights, 1U, pwm_bf);
 		}
 
 
