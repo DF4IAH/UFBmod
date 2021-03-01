61aca06ba8aaa9c964778e4aed8ed3215b103a54
Ulrich Habel <espero7757@gmx.net>
2021-03-01 09:32:54 +0100

    SW - DF4IAH_create_SREC.bat does chop off first line

--- SW/vitis_workspace/DF4IAH_create_SREC.bat
+++ SW/vitis_workspace/DF4IAH_create_SREC.bat
@@ -1,3 +1,5 @@
 
-F:/Xilinx/Vitis/2020.1/gnu/microblaze/nt/bin/mb-objcopy.EXE -O srec rtos_ufbmod/Debug/rtos_ufbmod.elf rtos_ufbmod/Debug/rtos_ufbmod.srec
-
+F:/Xilinx/Vitis/2020.1/gnu/microblaze/nt/bin/mb-objcopy.EXE -O srec rtos_ufbmod/Debug/rtos_ufbmod.elf rtos_ufbmod/Debug/rtos_ufbmod.srec1
+echo -e "\r" > rtos_ufbmod/Debug/rtos_ufbmod.srec
+tail -n +2  rtos_ufbmod/Debug/rtos_ufbmod.srec1 >> rtos_ufbmod/Debug/rtos_ufbmod.srec
+rm rtos_ufbmod/Debug/rtos_ufbmod.srec1
